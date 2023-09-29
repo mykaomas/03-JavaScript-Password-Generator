fda934322d299941e44285f3dd825803969b80b9
matronae <mykaomas@gmail.com>
2023-09-29 13:33:10 -0500

    Created arrays for the charachters needed for password; declared global variables; created function to generate passwoed; relinked the CSS filefor correct styling from the example image

---
+++ script.js
@@ -0,0 +1,40 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Create arrays for characters needed in password
+const specialCharacters = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '=', '{', '}', '|', ':', ';', '<', ',', '>', '.', '?', '/']
+console.log(specialCharacters)
+
+const upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+console.log(upperCaseLetters)
+
+const lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+console.log(lowerCaseLetters)
+
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+console.log(numbers)
+
+//Declare global variable 
+var userPassword
+var randomPassword
+
+//Create function to generate password
+function generatePassword() {
+
+  //Confirm button was pressed 
+  console.log("Button was pressed")
+
+  //Confirm  
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
